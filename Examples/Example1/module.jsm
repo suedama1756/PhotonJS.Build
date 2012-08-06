ee31743b981ce96f9a1426f442ca3768b0dfa739
Jason Young <suedama1756@hotmail.com>
2012-08-06 22:42:26 +0100

    Started adding tests, better late than never

--- Examples/Example1/module.jsm
+++ Examples/Example1/module.jsm
@@ -1,18 +1,15 @@
 /** @namespace module */
 ({
-    name : 'module',
-    files : [
+    name:'module',
+    files:[
         'Nested\\Foo\\file01.js',
         'file01.js',
         'file02.js'
     ],
-    dependencies : [
-        {
-            name : '$',
-            module : {
-                amd: 'jquery',
-                global : 'jQuery'
-            }
+    dependencies:{
+        '$':{
+            amd:'jquery',
+            global:'jQuery'
         }
-    ]
+    }
 });
---
+++ tests/build/module-wrapper-generator-tests.js
@@ -0,0 +1,169 @@
+var _build = require('../../node_modules/build/build.js');
+
+// Define window so we can hang stuff off it using eval
+var window;
+
+var basicModule = {
+    name:'module',
+    dependencies:{
+        '$':{
+            amd:'jquery',
+            global:'jQuery'
+        }
+    }
+};
+
+var basicModuleContent = [
+    'var data = "";',
+    'module.getData = function() { return data; };',
+    'module.setData = function(value) { data = value; };',
+    'factoryParameters["$"] = $;'];
+
+function generateModule(module, options, content) {
+    var generator = new _build.ModuleWrapperGenerator(module, options),
+        result = generator.generate(content);
+    console.log(result);
+    return result;
+}
+
+function generateAndEvaluateModule(module, options, content) {
+    var moduleText = generateModule(module, options, content);
+    eval(moduleText);
+}
+
+var define;
+var defineDependencies;
+var factoryParameters;
+var defineLog;
+
+function resolveDependencies(dependencies) {
+    return dependencies.map(function(dependency){
+        if (dependency === 'exports') {
+            return {};
+        } else {
+            return {
+                name : dependency
+            }
+        }
+    });
+}
+
+function mockAmd() {
+    // define log
+    defineLog = [];
+
+    // define mapped dependencies (object that holds the dependencies as they are known by the internals of the module)
+    factoryParameters = {};
+
+    // mock define
+    define = function (dependencies, factory) {
+        defineLog.push(
+            {
+                dependencies:dependencies,
+                factory:factory
+            });
+        factory.apply(null, defineDependencies = resolveDependencies(dependencies));
+    };
+    define.amd = {};
+    return {
+        close:function () {
+            define = undefined;
+            defineLog = undefined;
+            defineDependencies = undefined;
+        }
+    }
+}
+
+function mockGlobal() {
+    // define window
+    window = {
+        jQuery : {
+            name : 'jquery'
+        }
+    };
+
+    // define mapped dependencies (object that holds the dependencies as they are known by the internals of the module)
+    factoryParameters = {};
+
+    return {
+        close:function () {
+            window = null;
+        }
+    }
+}
+
+function mockAmdAndGlobal() {
+    var mockAmdHandle = mockAmd(), mockGlobalHandle = mockGlobal();
+    return {
+        close : function() {
+            mockAmdHandle.close();
+            mockGlobalHandle.close();
+        }
+    }
+}
+
+exports['When generating AMD'] = {
+    setUp:function (callback) {
+        this.mock_ = mockAmd();
+        generateAndEvaluateModule(basicModule, { formats:['amd'] },
+            basicModuleContent);
+        callback();
+    },
+    tearDown:function (callback) {
+        this.mock_.close();
+        callback();
+    },
+    'Should invoke define with correct dependencies':function (test) {
+        test.deepEqual(defineLog[0].dependencies,
+            ['exports', 'jquery']);
+        test.done();
+    }
+};
+
+exports["When generating Global"] = {
+    setUp:function (callback) {
+        this.mock_ = mockGlobal();
+        generateAndEvaluateModule(basicModule, { formats:['global'] },
+            basicModuleContent);
+        callback();
+    },
+    tearDown : function(callback) {
+        this.mock_.close();
+        callback();
+    },
+    "Should define global namespace":function (test) {
+        test.ok(window.module,
+            'Global module has not been defined.');
+        test.done();
+    },
+    "Should name dependencies correctly in factory" : function(test) {
+        test.ok(factoryParameters['$'].name === 'jquery');
+        test.done();
+    },
+    "Should correctly attach content to the global namespace" : function(test) {
+        window.module.setData(1);
+        test.ok(window.module.getData() === 1,
+            'Module content has not been defined correctly.');
+        test.done();
+    }
+};
+
+exports['When using both AMD and Global'] = {
+    setUp : function(callback) {
+        this.mock_ = mockAmdAndGlobal();
+        generateAndEvaluateModule(basicModule, { formats:['amd', 'global'] },
+            basicModuleContent);
+        callback();
+    },
+    tearDown : function(callback) {
+        this.mock_.close();
+        callback();
+    },
+    'Should prioritise AMD over Global':function (test) {
+        // verify we prioritized AMD over Global
+        test.ok(!window.module);
+        test.deepEqual(defineLog[0].dependencies,
+            ['exports', 'jquery']);
+        test.done();
+    }
+};
